c384298656d722eeb8b7df5a3bcbe7ff4da41046
wljjlu <33533527+wljjlu@users.noreply.github.com>
2019-03-25 10:40:31 +0800

    Add files via upload

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -140,7 +140,7 @@ class Agent():
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, seed, mu=0., theta=0.5, sigma=0.05):
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.1):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
         self.theta = theta
